379fe8f9c38c654630f23cb99df51753cc4841cd
Gabriel Bernal <gbernal@redhat.com>
2022-10-21 12:51:00 +0200

    React app structure & reusable components

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,18 @@
+export const MovieList = () => {
+  return (
+    <ul>
+      <li>
+        <article>
+          <h3>Lord of the Rings</h3>
+          <button>Check times</button>
+        </article>
+      </li>
+      <li>
+        <article>
+          <h3>Back to the Future</h3>
+          <button>Check times</button>
+        </article>
+      </li>
+    </ul>
+  );
+};
